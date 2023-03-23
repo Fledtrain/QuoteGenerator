4134ae2ef2f161645d45659936a534f58a147bfc
Fledtrain <Joeyew2014@gmail.com>
2023-03-23 15:15:47 -0400

    Fixing bugs with it not displaying

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 const api = "https://api.quotable.io/random";
 
 const getQuote = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
-  return data;
+  author.innerHTML = data.author;
+  quote.innerHTML = data.content;
 };
-getQuote(api);
+
+document.addEventListener("DOMContentLoaded", () => {
+  getQuote(api);
+
+  const newQuoteBtn = document.querySelector("#new-quote-btn");
+  newQuoteBtn.addEventListener("click", () => {
+    getQuote(api);
+  });
+});
+
+const Tweet = () => {
+  window.open(
+    "https://twitter.com/intent/tweet?text=" +
+      quote.innerHTML +
+      "--" +
+      author.innerHTML,
+    "width=300",
+    "height=600"
+  );
+};
