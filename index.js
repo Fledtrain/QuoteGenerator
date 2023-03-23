af2e00149ebbf56abd69723de58a5842d3fdf8a0
Fledtrain <Joeyew2014@gmail.com>
2023-03-23 15:26:12 -0400

    Rewrote the async function

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const quote = document.getElementById("quote");
-const author = document.getElementById("author");
-const api = "https://api.quotable.io/random";
+window.onload = () => {
+  const quote = document.getElementById("quote");
+  const author = document.getElementById("author");
+  const api = "https://api.quotable.io/random";
 
-const getQuote = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  author.innerHTML = data.author;
-  quote.innerHTML = data.content;
-};
+  const getQuote = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    author.innerHTML = data.author;
+    quote.innerHTML = data.content;
+  };
 
-document.addEventListener("DOMContentLoaded", () => {
   getQuote(api);
-
   const newQuoteBtn = document.querySelector("#new-quote-btn");
   newQuoteBtn.addEventListener("click", () => {
     getQuote(api);
   });
-});
+};
 
 const Tweet = () => {
   window.open(
